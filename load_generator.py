20403377121d67469c420245f096e6549e6ff69c
pvell <79489362+pvell@users.noreply.github.com>
2023-06-06 11:44:27 -0400

    Update load_generator.py

--- load_generator.py
+++ load_generator.py
@@ -1,84 +1,110 @@
 import configparser
+import random
 import time
+import quickfix as fix
 
-class FIXSession:
-    def __init__(self, session_id, host, port, sender, target):
-        self.session_id = session_id
-        self.host = host
-        self.port = port
-        self.sender = sender
-        self.target = target
-        # Add any other necessary attributes for the session
+# Global variables
+sessions = {}
 
-    def establish_connection(self):
-        # Implement code to establish the FIX connection
-        print(f"Establishing connection for session {self.session_id}...")
+# Function to handle the fromAdmin message
+def fromAdmin(message, session):
+    global sessions
+    session_id = session.getSessionID()
+    incoming_msg_seq_num = int(message.getHeader().getField(34))
+    msg_type = message.getHeader().getField(35)
 
-    def close_connection(self):
-        # Implement code to close the FIX connection
-        print(f"Closing connection for session {self.session_id}...")
+    if msg_type == 'A':  # Logon message
+        if incoming_msg_seq_num == 1:
+            print(f"Session established for {session_id}")
+            sessions[session_id] = True
+    elif msg_type == '5':  # Logout message
+        print(f"Session disconnected for {session_id}")
+        sessions[session_id] = False
 
-    def generate_message(self):
-        # Implement code to generate a FIX message for the session
-        print(f"Generating message for session {self.session_id}...")
+# Function to generate a random message based on weightage
+def generate_message(template, message_weights, session_id):
+    # Calculate total weightage
+    total_weight = sum(message_weights.values())
+    
+    # Generate a random number within the total weightage
+    random_num = random.randint(1, total_weight)
+    
+    # Find the message type based on the random number
+    cumulative_weight = 0
+    for msg_type, weight in message_weights.items():
+        cumulative_weight += weight
+        if random_num <= cumulative_weight:
+            # Replace placeholders in the template with the message type and outgoing sequence number
+            message = template.replace('<MessageType>', msg_type)
+            message = message.replace('<SeqNum>', str(get_outgoing_seq_num(session_id)))
+            return message
 
-def load_generator(config_file):
-    # Read and parse the configuration from the file
-    config = configparser.ConfigParser()
-    config.read(config_file)
+# Function to get the outgoing sequence number for a session
+def get_outgoing_seq_num(session_id):
+    session = fix.Session.lookupSession(fix.SessionID(session_id))
+    if session is not None:
+        return session.getNextSenderMsgSeqNum()
 
-    # Configure the FIX connection settings
-    sessions = []
-    for section in config.sections():
-        session_id = section
-        host = config.get(section, 'host')
-        port = config.getint(section, 'port')
-        sender = config.get(section, 'sender')
-        target = config.get(section, 'target')
-        session = FIXSession(session_id, host, port, sender, target)
-        sessions.append(session)
+# Function to increment the outgoing sequence number for a session
+def increment_outgoing_seq_num(session_id):
+    session = fix.Session.lookupSession(fix.SessionID(session_id))
+    if session is not None:
+        session.incrementNextSenderMsgSeqNum()
 
-    # Configure the message rate
-    message_rate = config.getfloat('Load', 'message_rate')
+# Function to send messages at the specified rate for a duration
+def send_messages(template, message_weights, rate, duration):
+    start_time = time.time()
+    end_time = start_time + duration
 
-    # Establish connections for all sessions
-    for session in sessions:
-        session.establish_connection()
+    while time.time() < end_time:
+        for session_id in sessions:
+            if sessions[session_id]:
+                session = fix.Session.lookupSession(fix.SessionID(session_id))
+                if session is not None:
+                    message = fix.Message(generate_message(template, message_weights, session_id))
+                    fix.Session.sendToTarget(message, session_id)
+                    increment_outgoing_seq_num(session_id)
+                    time.sleep(1 / rate)
 
-    # Start the initial load generation
-    is_paused = False  # Variable to track the load generator's pause state
+# Read configuration from config.ini file
+config = configparser.ConfigParser()
+config.read('config.ini')
 
-    # Main loop for load generation
-    while True:
-        if not is_paused:
-            for session in sessions:
-                session.generate_message()
-                time.sleep(1 / message_rate)
-        else:
-            print("Load generation paused.")
-            print("To resume the load generation, enter 'r'.")
-            print("To change the message rate, enter 'c' followed by the new rate.")
-            print("To quit, enter 'q'.")
-            user_input = input()
+template_file = config.get('LoadGenerator', 'template_file')
+connection_config_file = config.get('LoadGenerator', 'connection_config_file')
+message_rate = float(config.get('LoadGenerator', 'message_rate'))
 
-            if user_input.lower() == 'r':
-                print("Resuming load generation...")
-                is_paused = False
-            elif user_input.lower().startswith('c'):
-                new_rate = float(user_input[1:].strip())
-                message_rate = new_rate
-                print("Changing the message rate to", message_rate)
-                print("Resuming load generation...")
-                is_paused = False
-            elif user_input.lower() == 'q':
-                break
-            else:
-                print("Invalid input. Please try again.")
+# Load the template file
+with open(template_file, 'r') as file:
+    template = file.read()
 
-    # Close connections for all sessions
-    for session in sessions:
-        session.close_connection()
+# Load message weights from config.ini
+message_weights = dict(config.items('MessageTypes'))
 
-# Example usage
-config_file = 'config.ini'
-load_generator(config_file)
+# Load connection configuration from connections.cfg file
+settings = fix.SessionSettings(connection_config_file)
+application = fix.Application()
+storeFactory = fix.FileStoreFactory(settings)
+logFactory = fix.ScreenLogFactory(settings)
+initiator = fix.SocketInitiator(application, storeFactory, settings, logFactory)
+
+# Start the FIX sessions
+initiator.start()
+
+# Get the session IDs
+session_ids = initiator.getSessions()
+
+# Initialize session state
+for session_id in session_ids:
+    sessions[session_id] = False
+
+# Wait for sessions to be established
+while not all(sessions.values()):
+    time.sleep(1)
+
+# Send messages
+send_duration = 60  # Duration in seconds
+send_messages(template, message_weights, message_rate, send_duration)
+
+# Stop the FIX sessions
+initiator.stop()
