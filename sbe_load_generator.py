e0c24210fd079ff336b98ad657b86cdfdd401e48
pvell <79489362+pvell@users.noreply.github.com>
2023-06-12 19:59:48 -0400

    Update sbe_load_generator.py

--- sbe_load_generator.py
+++ sbe_load_generator.py
@@ -1,30 +1,81 @@
-from sbe import Encoder, Decoder
-from sbe.schemaparser import SchemaParser
 
-# Load the SBE schema file
-schema_file = 'path/to/SBE.xml'
-parser = SchemaParser()
-sbe_schema = parser.parse_file(schema_file)
+import configparser
+import socket
+import time
+from sbe-tool import encode_message
 
-# Create an encoder and decoder based on the SBE schema
-encoder = Encoder(sbe_schema)
-decoder = Decoder(sbe_schema)
+config = configparser.ConfigParser()
+config.read('config.ini')
 
-# Example encoding and decoding
-input_message = {
-    'field1': 123,
-    'field2': 'example',
-    # ...
-}
+# Load the schema for encoding and decoding messages
+schema_file = 'sbe-schema.xml'
+schema = load_schema(schema_file)
 
-# Encode the message
-encoded_message = encoder.encode(input_message)
+# Get the default configuration for message rate and duration
+default_config = config['default']
+default_message_rate = int(default_config['message_rate'])
+default_duration = int(default_config['duration'])
 
-# Decode the message
-decoded_message = decoder.decode(encoded_message)
+# Loop through all the session configurations
+for session_name in config.sections():
+    if session_name == 'default':
+        continue
 
-# Print the input and decoded messages
-print("Input Message:")
-print(input_message)
-print("\nDecoded Message:")
-print(decoded_message)
+    # Get the session configuration
+    session_config = config[session_name]
+
+    # Extract the connection details
+    server_ip = session_config['ip']
+    server_port = int(session_config['port'])
+
+    # Establish the TCP connection
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((server_ip, server_port))
+
+    # Set the TCP connection to binary mode
+    client_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+
+   # Login process
+login_request_fields = {}
+login_request_fields[3] = 100  # Message Type
+login_request_fields[4] = len(connection_config['username'])  # Token Type
+login_request_fields[5] = connection_config['username']  # Token
+login_request_message = encode_message(schema, 'LoginRequest', login_request_fields)
+client_socket.sendall(login_request_message)
+
+# Receive the login response
+login_response_message = client_socket.recv(4096)
+_, login_response_fields = decode_message(schema, login_response_message)
+
+# Check if the login was accepted
+if login_response_fields.get(3) == 1:  # Login Accepted
+    print("Login accepted.")
+else:
+    print("Login rejected. Exiting.")
+    client_socket.close()
+    exit(1)
+
+    # Calculate message rate per session
+    message_rate = int(session_config.get('message_rate', default_message_rate))
+    duration = int(session_config.get('duration', default_duration))
+    message_interval = 1 / message_rate
+
+    # Send NewOrderSingle messages
+    start_time = time.time()
+    end_time = start_time + duration
+
+    while time.time() < end_time:
+        # Generate and send NewOrderSingle message
+        message_name = 'NewOrderSingle'
+        field_values = {
+            # ... (populate the required fields for NewOrderSingle)
+        }
+        encoded_message = encode_message(schema, message_name, field_values)
+        client_socket.sendall(encoded_message)
+
+        # Wait for the next message interval
+        time.sleep(message_interval)
+
+    # Close the TCP connection
+    client_socket.close()
+
