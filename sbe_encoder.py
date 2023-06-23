fee9ace4e2bccbd50a1756de5638076dfacc98ff
pvell <79489362+pvell@users.noreply.github.com>
2023-06-22 21:33:55 -0400

    Update sbe_encoder.py

--- sbe_encoder.py
+++ sbe_encoder.py
@@ -1,104 +1,131 @@
+import struct
 import xml.etree.ElementTree as ET
 
 def load_schema(schema_file):
     tree = ET.parse(schema_file)
     root = tree.getroot()
 
-    composite_dict = {}
-    type_dict = {}
+    messages = {}
 
-    # Load composite definitions
-    for composite_def in root.iter("composite"):
-        composite_id = int(composite_def.attrib["id"])
+    # Find the XML namespace
+    namespace = root.tag.split('}')[0] + '}'
+
+    type_counter = 1
+
+    # Load message definitions
+    for message_elem in root.iter(f'{namespace}message'):
+        message_id = int(message_elem.attrib["id"])
+        message_name = message_elem.attrib["name"]
         fields = {}
-        for field_def in composite_def.iter("field"):
-            field_id = int(field_def.attrib["id"])
-            field_name = field_def.attrib["name"]
-            fields[field_id] = field_name
-        composite_dict[composite_id] = fields
+
+        for field_elem in message_elem.iter(f'field'):
+            field_id = int(field_elem.attrib["id"])
+            field_name = field_elem.attrib["name"]
+            field_type = field_elem.attrib.get("type")
+            fields[field_id] = {
+                "name": field_name,
+                "type": field_type
+            }
+
+        messages[message_name] = {
+            "id": message_id,
+            "fields": fields
+        }
 
     # Load type definitions
-    for type_def in root.iter("type"):
-        type_id = int(type_def.attrib["id"])
-        type_name = type_def.attrib["name"]
-        type_dict[type_id] = type_name
+    for type_elem in root.iter(f'type'):
+        type_name = type_elem.attrib["name"]
+        type_fields = {}
 
-    return composite_dict, type_dict
+        for sub_type_elem in type_elem.iter(f'type'):
+            sub_type_name = sub_type_elem.attrib["name"]
+            sub_type_primitive = sub_type_elem.attrib.get("primitiveType")
+            type_fields[sub_type_name] = sub_type_primitive
+
+        if not type_fields:
+            type_fields[type_name] = type_elem.text
+
+        messages[type_name] = {
+            "id": type_counter,
+            "fields": type_fields
+        }
+
+        type_counter += 1
+
+    return messages
+
 
 def encode_message(schema_file, message_name, field_values):
-    composite_dict, type_dict = load_schema(schema_file)
-    print(composite_dict, type_dict)
-    composite_fields = composite_dict.get(message_name)
-    if not composite_fields:
+    schema = load_schema(schema_file)
+    message = schema.get(message_name)
+    if not message:
         raise ValueError(f"Invalid message name '{message_name}'")
 
     encoded_fields = []
-    for field_id, field_name in composite_fields:
-        field_value = field_values.get(field_id, None)
-        encoded_field = encode_field(field_id, field_value, type_dict, composite_dict)
+    for field_id, field_data in message['fields'].items():
+        field_name = field_data['name']
+        field_type = field_data['type']
+        field_value = field_values.get(field_name, None)
+        encoded_field = encode_field(field_id, field_value, field_type, schema, field_values)
         encoded_fields.append(encoded_field)
 
-    encoded_message = "".join(encoded_fields)
+    encoded_message = b''.join(encoded_fields)
     return encoded_message
 
-def encode_field(field_id, field_value, type_dict, composite_dict):
-    type_name = type_dict.get(field_id)
-    if not type_name:
-        raise ValueError(f"Invalid field ID '{field_id}'")
 
-    encoded_value = ""
-    if type_name == "string":
-        encoded_value = str(field_value) if field_value is not None else ""
-    elif type_name == "int":
-        encoded_value = str(field_value) if field_value is not None else "0"
-    elif type_name == "float":
-        encoded_value = str(field_value) if field_value is not None else "0.0"
-    elif type_name == "composite":
-        composite_fields = composite_dict.get(field_name)
+def encode_field(tag, value, field_type, schema, field_values):
+    encoded_value = b''
+
+    if field_type == 'int':
+        encoded_value = struct.pack('!i', int(value))
+    elif field_type == 'uint':
+        encoded_value = struct.pack('!I', int(value))
+    elif field_type == 'float':
+        encoded_value = struct.pack('!f', float(value))
+    elif field_type == 'double':
+        encoded_value = struct.pack('!d', float(value))
+    elif field_type == 'char':
+        value = value if value is not None else ''
+        encoded_value = struct.pack('!s', value.encode())
+    elif field_type == 'boolean':
+        encoded_value = struct.pack('!?', bool(value))
+    elif field_type.startswith('group'):
+        group_fields = field_type.split(':')[1:]
+        for group_field in group_fields:
+            group_tag, group_value = group_field.split('=')
+            encoded_group_field = encode_field(int(group_tag), group_value, field_type, schema, field_values)
+            encoded_value += encoded_group_field
+    elif field_type.startswith('composite'):
+        composite_fields = schema.get(field_type.split(':')[1])
         if not composite_fields:
-            raise ValueError(f"Invalid composite field '{field_name}'")
-        for sub_field_id, sub_field_name in composite_fields:
-            sub_field_value = field_value.get(sub_field_id, None)
-            encoded_sub_field = encode_field(sub_field_id, sub_field_value, type_dict, composite_dict)
+            raise ValueError(f"Invalid composite field '{field_type}'")
+        for sub_field_data in composite_fields['fields'].values():
+            sub_field_id = sub_field_data['id']
+            sub_field_type = sub_field_data['type']
+            sub_field_name = sub_field_data['name']
+            sub_field_value = field_values.get(sub_field_name, None)
+            encoded_sub_field = encode_field(sub_field_id, sub_field_value, sub_field_type, schema, field_values)
             encoded_value += encoded_sub_field
 
-    print(f"Encoded field {field_id}: {encoded_value}")
     return encoded_value
 
+
 # Example usage
-schema_file = "sbe-schema-options.xml"
-message_name = "NewOrderSingle"
+schema_file = 'sbe-schema-options.xml'
+message_name = 'NewOrderSingle'
 field_values = {
-    52: 1623322435000000000,
-    11: "CLORD12345",
-    21035: "SECURITY123",
-    54: "BUY",
-    38: 100,
-    40: "LIMIT",
-    44: 99.99,
-    59: "DAY",
-    77: "OPEN",
-    18: "ADD",
-    1815: "MEDIUM",
-    21020: 1,
-    21021: 2,
-    2362: 123,
-    21001: "PREVENT",
-    21000: 456,
-    21005: 789,
-    453: [
-        {
-            448: "PARTY1",
-            447: "SRC1",
-            452: 1
-        },
-        {
-            448: "PARTY2",
-            447: "SRC2",
-            452: 2
-        }
-    ]
+    'ClOrdID': 'CLORD12345',
+    'MPID': 'MPID123',
+    'Symbol': 'AAPL',
+    'SymbolSfx': None,
+    'Side': 'BUY',
+    'OrderQty': 100,
+    'OrdType': 'LIMIT',
+    'Price': 99.99,
+    'TimeInForce': 'DAY',
+    'OrderCapacity': 'AGENCY',
+    'CustOrderCapacity': 'RETAIL'
 }
 
 encoded_message = encode_message(schema_file, message_name, field_values)
-print("Encoded Message:", encoded_message)
+print('Encoded Message:', encoded_message)
