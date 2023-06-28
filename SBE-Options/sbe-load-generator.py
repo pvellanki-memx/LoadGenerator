0944962e99f8e30de0588867e7e1502cdcd79a15
pvell <79489362+pvell@users.noreply.github.com>
2023-06-28 14:49:18 -0400

    Update sbe-load-generator.py

--- SBE-Options/sbe-load-generator.py
+++ SBE-Options/sbe-load-generator.py
@@ -2,13 +2,13 @@ import configparser
 import socket
 import struct
 import time
-from sbe-encoder-decoder import UTCTimestampNanos, NewOrderSingle, ShortTwoSideBulkQuote, LongTwoSideBulkQuote, ShortOneSideBulkQuote, LongOneSideBulkQuote
-from sbe-encoder-decoder import UINT32, OrdType, TimeInForceType, ExecInstType, TradingCapacityType, SideType
+from sbe_encoder_decoder import UTCTimestampNanos, NewOrderSingle, ShortTwoSideBulkQuote, LongTwoSideBulkQuote, ShortOneSideBulkQuote, LongOneSideBulkQuote
+from sbe_encoder_decoder import UINT32, OrdType, TimeInForceType, ExecInstType, TradingCapacityType, SideType, Party
 from random import choices, randint
 import string
 from random import choices, randint
 
-from sbe-encoder-decoder import *
+
 
 # Read configuration from config.ini
 config = configparser.ConfigParser()
@@ -25,11 +25,6 @@ user = connection_config['Session1']['user']
 password = connection_config['Session1']['password']
 token = f'{user}:{password}'
 
-# Token and header details
-token_type = 'P'  # Assuming token type is always 'P'
-token_length = len(token)
-header = struct.pack('!BHB', 100, token_length + 1, token_type.encode('utf-8')[0])
-message = header + token.encode('utf-8')
 
 # Read other configuration parameters
 message_rate = config.getint('Load', 'message_rate')
@@ -53,6 +48,14 @@ with open(template_file) as template_file:
 
 # Establish SBE TCP session
 def establish_session():
+
+        # Login request
+    message_type = 100
+    token_type = 'P'  # Assuming token type is always 'P'
+    token_length = len(token)
+    header = struct.pack('!BHB', message_type, token_length + 1, token_type.encode('utf-8')[0])
+    message = header + token.encode('utf-8')
+
     # Create a socket and establish the connection
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((host, port))
@@ -60,12 +63,13 @@ def establish_session():
     # Set socket to binary mode
     client_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
 
-    # Send the login request
+        # Send the login request
     client_socket.sendall(message)
 
     # Receive and handle the response
-    response_header = client_socket.recv(2)
-    response_type, response_length = struct.unpack('!BB', response_header)
+    response_header = client_socket.recv(3)
+    response_type, response_length = struct.unpack('!B H', response_header)
+    print(response_type, response_length)
 
     if response_type == 1:  # Login Accepted
         response_message = client_socket.recv(response_length)
@@ -83,27 +87,30 @@ def establish_session():
 
     # Continue with the session
     while True:
-        response_header = client_socket.recv(2)
-        response_type, response_length = struct.unpack('!BB', response_header)
+        response_header = client_socket.recv(11)
+        response_type, response_length, session_id = struct.unpack('!B H Q', response_header)
+        print(response_header, response_type, response_length)
 
         if response_type == 3:  # Start of Session
-            response_message = client_socket.recv(response_length)
-            session_id = struct.unpack('!Q', response_message[3:11])[0]
             print("Start of Session. Session ID:", session_id)
             break
         else:
             print("Invalid response received.")
 
-    # Send the Stream Request
+
+    # Stream Request
     stream_request_type = 103
     stream_request_length = 16  # 4 bytes for message type and length, 8 bytes for session ID, 8 bytes for next sequence number
     NEXT_SEQUENCE_NUMBER = 0
-    stream_request_header = struct.pack('!BHBQQ', stream_request_type, stream_request_length, session_id, NEXT_SEQUENCE_NUMBER)
+    stream_request_header = struct.pack('!BHQQ', stream_request_type, stream_request_length, session_id, NEXT_SEQUENCE_NUMBER)
     stream_request = stream_request_header
+
+    # Send the Stream Request
     client_socket.sendall(stream_request)
 
-    response_header = client_socket.recv(2)
-    response_type, response_length = struct.unpack('!BB', response_header)
+    response_header = client_socket.recv(3)
+    response_type, response_length = struct.unpack('!B H', response_header)
+    print(response_type, response_length)
 
     if response_type == 9:  # Stream Rejected
         response_message = client_socket.recv(response_length)
@@ -116,8 +123,14 @@ def establish_session():
         print("End of Stream")
         client_socket.close()
         exit()
+    elif response_type == 8:  # Stream Begin
+        response_message = client_socket.recv(response_length - 2)
+        print(response_message)
+        NEXT_SEQUENCE_NUMBER  = struct.unpack('!Q', response_message[3:11])
+        print("Stream Begin received")
     else:
-        print("Invalid response received.")
+        print("Invalid Message received")
+    
 
     return client_socket, session_id
 
@@ -126,31 +139,45 @@ def generate_message_type():
     message_types = list(weights.keys())
     return choices(message_types, weights=list(weights.values()), k=1)[0]
 
-def generate_message(message_type)
+def generate_message(message_type):
     if message_type == 'NewOrderSingle':
-        sending_time = UTCTimestampNanos()
+        # Generate values for the fields
+        sending_time = UTCTimestampNanos(int(time.time() * 10**9))
         cl_ord_id = ''.join(choices(string.ascii_uppercase + string.digits, k=20))
         options_security_id = choices(security_ids)[0]
-        side = SideType.BUY  # Assuming a BUY order, you can customize this
-        order_qty = UINT32(randint(1, 100))
-        ord_type = OrdType.LIMIT  # Assuming a LIMIT order, you can customize this
-        time_in_force = TimeInForceType.GOOD_TILL_CANCEL  # Assuming a GTC order, you can customize this
-        exec_inst = [ExecInstType.PRIMARY_PEG, ExecInstType.DISPLAY_PRICE]  # Assuming primary peg and display price instructions, you can customize this
-        trading_capacity = TradingCapacityType.PRINCIPAL  # Assuming principal capacity, you can customize this
-    
-        # Create an instance of NewOrderSingle and set the field values
-        new_order_single = NewOrderSingle()
-        new_order_single.sending_time = sending_time
-        new_order_single.cl_ord_id = cl_ord_id
-        new_order_single.options_security_id = options_security_id
-        new_order_single.side = side
-        new_order_single.order_qty = order_qty
-        new_order_single.ord_type = ord_type
-        new_order_single.time_in_force = time_in_force
-        new_order_single.exec_inst = exec_inst
-        new_order_single.trading_capacity = trading_capacity
+        side = SideType(value=SideType.BUY)  # Set the side to "Buy"
+        order_qty = UINT32(value=randint(1, 100))
+        ord_type = OrdType(value=OrdType.LIMIT)  # Set the order type to "Limit"
+        time_in_force = TimeInForceType(value=TimeInForceType.DAY)  # Set the time in force to "Day"
+        exec_inst = ExecInstType(value=ExecInstType.ParticipateDoNotInitiate)  # Set the execution instructions
+        trading_capacity = TradingCapacityType(value=TradingCapacityType.CUSTOMER)  # Set the trading capacity
+        parties = [Party(party_id='EFID', party_id_source='D', party_role='CUSTOMER')]  # Set the parties
 
-        return new_order_single
+        # Create an instance of NewOrderSingle and set the field values
+        new_order_single = NewOrderSingle(
+            sending_time=sending_time,
+            cl_ord_id=cl_ord_id,
+            options_security_id_35=options_security_id,
+            side=side,
+            order_qty=order_qty,
+            ord_type=ord_type,
+            time_in_force=time_in_force,
+            exec_inst=exec_inst,
+            trading_capacity=trading_capacity,
+            parties=parties
+        )
+
+        unsequenced_message = struct.pack('!BH', 104, 102)  # MessageType=104, MessageLength=6, TCP Header Length=102
+        # Encode the NewOrderSingle instance
+        encoded_message = new_order_single.encode()
+        message = unsequenced_message + encoded_message
+
+        # Print the encoded message
+        print(encoded_message)
+
+        return message
+
+        
 
 
     elif message_type == 'ShortTwoSideBulkQuote':
@@ -182,10 +209,10 @@ def generate_message(message_type)
 
 # Send the generated message over the TCP connection
 def send_message(client_socket, message):
-    encoded_message = message.encode()
+
 
     # Send the message
-    client_socket.sendall(encoded_message)
+    client_socket.sendall(message)
 
     # Sleep for a short period to control the message rate
     time.sleep(1 / message_rate)
@@ -209,8 +236,22 @@ def main():
         # Send the generated message over the TCP connection
         send_message(client_socket, message)
 
+        # Sleep for a short period to control the message rate
+        time.sleep(1 / message_rate)
+
     # Close the TCP connection
     client_socket.close()
 
 if __name__ == '__main__':
     main()
+
+
+
+
+
+
+
+
+
+
+
