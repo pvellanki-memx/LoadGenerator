fd167834bf6654cc061794ae2739d1fc7e4d328b
pvell <79489362+pvell@users.noreply.github.com>
2023-07-04 15:21:33 -0400

    Update neworder_replace_cancel.py

--- FIX_Functional/neworder_replace_cancel.py
+++ FIX_Functional/neworder_replace_cancel.py
@@ -1,256 +1,134 @@
 import unittest
-import fix
 import time
+import fix
+from fix_validator import validate_execution_report, replace_placeholders
 
+class TestFIXSession(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.app = fix.Application()
+        cls.settings = fix.SessionSettings("config.cfg")
+        cls.storeFactory = fix.FileStoreFactory(cls.settings)
+        cls.logFactory = fix.ScreenLogFactory(cls.settings)
+        cls.initiator = fix.SocketInitiator(cls.app, cls.storeFactory, cls.settings, cls.logFactory)
+        cls.initiator.start()
+        time.sleep(1)
+        cls.session_id = cls.app.session_id
 
-def replace_placeholders(template_message, placeholders):
-    """
-    Replaces the placeholders in the given template message with the provided values.
-    """
-    replaced_message = template_message
-    for placeholder, value in placeholders.items():
-        replaced_message = replaced_message.replace(placeholder, value)
-    return replaced_message
+    @classmethod
+    def tearDownClass(cls):
+        cls.initiator.stop()
 
+    def send_new_order_single(self, order_type, time_in_force, order_qty, symbol, exec_inst=''):
+        with open(self.app.template_file, 'r') as file:
+            template_messages = file.readlines()
 
-def validate_execution_report(exec_report, order_qty, symbol, order_type, tif, price):
-    """
-    Validates the fields on the execution report against the provided values.
-    """
-    # Check OrderQty
-    received_order_qty = exec_report.getField(38)
-    assert received_order_qty == str(order_qty), f"Expected OrderQty: {order_qty}, Received: {received_order_qty}"
+        for template_message in template_messages:
+            placeholders = {
+                '<ClOrdID>': self.app.generate_clordid(),
+                '<OrderQty>': str(order_qty),
+                '<Symbol>': symbol,
+                '<OrdType>': order_type,
+                '<TimeInForce>': time_in_force,
+                '<ExecInst>': exec_inst
+            }
+            message = replace_placeholders(template_message, placeholders)
 
-    # Check Symbol
-    received_symbol = exec_report.getField(55)
-    assert received_symbol == symbol, f"Expected Symbol: {symbol}, Received: {received_symbol}"
+            new_order_message = fix.Message()
+            new_order_message.setString(message, False, self.app.message_factory)
+            fix.Session.sendToTarget(new_order_message, self.session_id)
 
-    # Check OrdType
-    received_order_type = exec_report.getField(40)
-    assert received_order_type == order_type, f"Expected OrdType: {order_type}, Received: {received_order_type}"
+            time.sleep(2)
 
-    # Check TimeInForce
-    received_tif = exec_report.getField(59)
-    assert received_tif == tif, f"Expected TimeInForce: {tif}, Received: {received_tif}"
+            self.assertTrue(self.app.execution_report_received)
+            exec_report = self.app.last_execution_report
+            cl_ord_id = exec_report.getField(11)
 
-    # Check Price
-    received_price = exec_report.getField(44)
-    assert received_price == str(price), f"Expected Price: {price}, Received: {received_price}"
+            validate_execution_report(exec_report, order_qty=order_qty, symbol=symbol, order_type=order_type, tif=time_in_force)
+            self.assertEqual(exec_report.getField(39), '0')
 
+    def send_cancel_replace_request(self, orig_cl_ord_id, new_price=None, new_order_qty=None):
+        with open(self.app.template_file, 'r') as file:
+            template_messages = file.readlines()
 
-class MyTestCase(unittest.TestCase):
-    def setUp(self):
-        self.app = MyApplication('template_file.txt', read_config_file('config.ini'))
+        for template_message in template_messages:
+            placeholders = {
+                '<ClOrdID>': self.app.generate_clordid(),
+                '<OrigClOrdID>': orig_cl_ord_id,
+                '<Price>': str(new_price) if new_price else '',
+                '<OrderQty>': str(new_order_qty) if new_order_qty else ''
+            }
+            message = replace_placeholders(template_message, placeholders)
 
-        settings = fix.SessionSettings('connections.cfg')
-        store_factory = fix.FileStoreFactory(settings)
-        log_factory = fix.FileLogFactory(settings)
-        self.initiator = fix.SocketInitiator(self.app, store_factory, settings, log_factory)
-        self.initiator.start()
-        time.sleep(2)
+            cancel_replace_message = fix.Message()
+            cancel_replace_message.setString(message, False, self.app.message_factory)
+            fix.Session.sendToTarget(cancel_replace_message, self.session_id)
+
+            time.sleep(2)
+
+            self.assertTrue(self.app.execution_report_received)
+            exec_report = self.app.last_execution_report
+            cl_ord_id = exec_report.getField(11)
+
+            validate_execution_report(exec_report, order_qty=new_order_qty, symbol='', order_type='', tif='')
+            self.assertEqual(exec_report.getField(39), '0')
+
+    def send_order_cancel_request(self, orig_cl_ord_id):
+        with open(self.app.template_file, 'r') as file:
+            template_messages = file.readlines()
+
+        for template_message in template_messages:
+            placeholders = {
+                '<ClOrdID>': self.app.generate_clordid(),
+                '<OrigClOrdID>': orig_cl_ord_id
+            }
+            message = replace_placeholders(template_message, placeholders)
+
+            order_cancel_message = fix.Message()
+            order_cancel_message.setString(message, False, self.app.message_factory)
+            fix.Session.sendToTarget(order_cancel_message, self.session_id)
+
+            time.sleep(2)
+
+            self.assertTrue(self.app.execution_report_received)
+            exec_report = self.app.last_execution_report
+            cl_ord_id = exec_report.getField(11)
+
+            validate_execution_report(exec_report, order_qty=0, symbol='', order_type='', tif='')
+            self.assertEqual(exec_report.getField(39), '4')
+
+    def test_market_order_with_day_tif(self):
+        self.send_new_order_single(order_type='1', time_in_force='0', order_qty=100, symbol='AAPL')
+
+    def test_market_order_with_ioc_tif(self):
+        self.send_new_order_single(order_type='1', time_in_force='3', order_qty=100, symbol='AAPL')
+
+    def test_limit_order_with_day_tif(self):
+        self.send_new_order_single(order_type='2', time_in_force='0', order_qty=100, symbol='AAPL')
+
+    def test_limit_order_with_ioc_tif(self):
+        self.send_new_order_single(order_type='2', time_in_force='3', order_qty=100, symbol='AAPL')
+
+    def test_limit_order_with_iso_modifier(self):
+        self.send_new_order_single(order_type='2', time_in_force='0', order_qty=100, symbol='AAPL', exec_inst='F')
+
+    def test_rejected_market_order_with_iso_modifier(self):
+        self.send_new_order_single(order_type='1', time_in_force='0', order_qty=100, symbol='AAPL', exec_inst='F')
+        self.assertEqual(self.app.last_execution_report.getField(39), '8')
+
+    def test_cancel_replace_price(self):
+        self.send_new_order_single(order_type='2', time_in_force='0', order_qty=100, symbol='AAPL')
+        orig_cl_ord_id = self.app.last_execution_report.getField(11)
+        self.send_cancel_replace_request(orig_cl_ord_id, new_price=150)
+
+    def test_cancel_replace_qty(self):
+        self.send_new_order_single(order_type='2', time_in_force='0', order_qty=100, symbol='AAPL')
+        orig_cl_ord_id = self.app.last_execution_report.getField(11)
+        self.send_cancel_replace_request(orig_cl_ord_id, new_order_qty=50)
 
     def tearDown(self):
-        self.initiator.stop()
-        time.sleep(2)
-
-    def test_send_market_order_with_DAY(self):
-        # Read template messages from file
-        with open(self.app.template_file, 'r') as file:
-            template_messages = file.readlines()
-
-        # Iterate over template messages
-        for template_message in template_messages:
-            # Replace placeholders in the message template
-            placeholders = {'<ClOrdID>': self.app.generate_clordid()}
-            message = replace_placeholders(template_message, placeholders)
-
-            # Create and send the NewOrderSingle message with Market order type and DAY time-in-force
-            new_order_message = fix.Message()
-            new_order_message.setString(message, False, self.app.message_factory)
-            new_order_message.setField(40, '1')  # Market order type
-            new_order_message.setField(59, '0')  # DAY time-in-force
-            fix.Session.sendToTarget(new_order_message, self.session_id)
-
-            # Wait for execution report
-            time.sleep(2)
-
-            # Assert that the execution report was received
-            self.assertTrue(self.app.execution_report_received)
-
-            # Get the ClOrdID from the execution report
-            exec_report = self.app.last_execution_report
-            cl_ord_id = exec_report.getField(11)
-
-            # Validate the fields on the execution report
-            validate_execution_report(exec_report, order_qty=100, symbol='AAPL', order_type='1', tif='0', price=0.0)
-
-            # Add assertions or further verifications as needed
-
-    def test_send_market_order_with_IOC(self):
-        # Read template messages from file
-        with open(self.app.template_file, 'r') as file:
-            template_messages = file.readlines()
-
-        # Iterate over template messages
-        for template_message in template_messages:
-            # Replace placeholders in the message template
-            placeholders = {'<ClOrdID>': self.app.generate_clordid()}
-            message = replace_placeholders(template_message, placeholders)
-
-            # Create and send the NewOrderSingle message with Market order type and IOC time-in-force
-            new_order_message = fix.Message()
-            new_order_message.setString(message, False, self.app.message_factory)
-            new_order_message.setField(40, '1')  # Market order type
-            new_order_message.setField(59, '3')  # IOC time-in-force
-            fix.Session.sendToTarget(new_order_message, self.session_id)
-
-            # Wait for execution report
-            time.sleep(2)
-
-            # Assert that the execution report was received
-            self.assertTrue(self.app.execution_report_received)
-
-            # Get the ClOrdID from the execution report
-            exec_report = self.app.last_execution_report
-            cl_ord_id = exec_report.getField(11)
-
-            # Validate the fields on the execution report
-            validate_execution_report(exec_report, order_qty=100, symbol='AAPL', order_type='1', tif='3', price=0.0)
-
-            # Add assertions or further verifications as needed
-
-    def test_send_limit_order_with_DAY(self):
-        # Read template messages from file
-        with open(self.app.template_file, 'r') as file:
-            template_messages = file.readlines()
-
-        # Iterate over template messages
-        for template_message in template_messages:
-            # Replace placeholders in the message template
-            placeholders = {'<ClOrdID>': self.app.generate_clordid()}
-            message = replace_placeholders(template_message, placeholders)
-
-            # Create and send the NewOrderSingle message with Limit order type and DAY time-in-force
-            new_order_message = fix.Message()
-            new_order_message.setString(message, False, self.app.message_factory)
-            new_order_message.setField(40, '2')  # Limit order type
-            new_order_message.setField(59, '0')  # DAY time-in-force
-            fix.Session.sendToTarget(new_order_message, self.session_id)
-
-            # Wait for execution report
-            time.sleep(2)
-
-            # Assert that the execution report was received
-            self.assertTrue(self.app.execution_report_received)
-
-            # Get the ClOrdID from the execution report
-            exec_report = self.app.last_execution_report
-            cl_ord_id = exec_report.getField(11)
-
-            # Validate the fields on the execution report
-            validate_execution_report(exec_report, order_qty=100, symbol='AAPL', order_type='2', tif='0', price=100.0)
-
-            # Add assertions or further verifications as needed
-
-    def test_send_limit_order_with_IOC(self):
-        # Read template messages from file
-        with open(self.app.template_file, 'r') as file:
-            template_messages = file.readlines()
-
-        # Iterate over template messages
-        for template_message in template_messages:
-            # Replace placeholders in the message template
-            placeholders = {'<ClOrdID>': self.app.generate_clordid()}
-            message = replace_placeholders(template_message, placeholders)
-
-            # Create and send the NewOrderSingle message with Limit order type and IOC time-in-force
-            new_order_message = fix.Message()
-            new_order_message.setString(message, False, self.app.message_factory)
-            new_order_message.setField(40, '2')  # Limit order type
-            new_order_message.setField(59, '3')  # IOC time-in-force
-            fix.Session.sendToTarget(new_order_message, self.session_id)
-
-            # Wait for execution report
-            time.sleep(2)
-
-            # Assert that the execution report was received
-            self.assertTrue(self.app.execution_report_received)
-
-            # Get the ClOrdID from the execution report
-            exec_report = self.app.last_execution_report
-            cl_ord_id = exec_report.getField(11)
-
-            # Validate the fields on the execution report
-            validate_execution_report(exec_report, order_qty=100, symbol='AAPL', order_type='2', tif='3', price=100.0)
-
-            # Add assertions or further verifications as needed
-
-    def test_send_limit_order_with_ISO(self):
-        # Read template messages from file
-        with open(self.app.template_file, 'r') as file:
-            template_messages = file.readlines()
-
-        # Iterate over template messages
-        for template_message in template_messages:
-            # Replace placeholders in the message template
-            placeholders = {'<ClOrdID>': self.app.generate_clordid()}
-            message = replace_placeholders(template_message, placeholders)
-
-            # Create and send the NewOrderSingle message with Limit order type and ISO modifier
-            new_order_message = fix.Message()
-            new_order_message.setString(message, False, self.app.message_factory)
-            new_order_message.setField(40, '2')  # Limit order type
-            new_order_message.setField(18, 'f')  # ISO modifier
-            fix.Session.sendToTarget(new_order_message, self.session_id)
-
-            # Wait for execution report
-            time.sleep(2)
-
-            # Assert that the execution report was received
-            self.assertTrue(self.app.execution_report_received)
-
-            # Get the ClOrdID from the execution report
-            exec_report = self.app.last_execution_report
-            cl_ord_id = exec_report.getField(11)
-
-            # Validate the fields on the execution report
-            validate_execution_report(exec_report, order_qty=100, symbol='AAPL', order_type='2', tif='0', price=100.0)
-            self.assertEqual(exec_report.getField(18), 'f')  # Check if ISO modifier is present
-
-          
-
-def test_send_market_order_ISO_rejected(self):
-    # Read template messages from file
-    with open(self.app.template_file, 'r') as file:
-        template_messages = file.readlines()
-
-    # Iterate over template messages
-    for template_message in template_messages:
-        # Replace placeholders in the message template
-        placeholders = {'<ClOrdID>': self.app.generate_clordid()}
-        message = replace_placeholders(template_message, placeholders)
-
-        # Create and send the NewOrderSingle message with Market order type
-        new_order_message = fix.Message()
-        new_order_message.setString(message, False, self.app.message_factory)
-        new_order_message.setField(40, '1')  # Market order type
-        fix.Session.sendToTarget(new_order_message, self.session_id)
-
-        # Wait for execution report
-        time.sleep(2)
-
-        # Assert that the execution report was received
-        self.assertTrue(self.app.execution_report_received)
-
-        # Get the ClOrdID from the execution report
-        exec_report = self.app.last_execution_report
-        cl_ord_id = exec_report.getField(11)
-
-        # Validate the fields on the execution report
-        validate_execution_report(exec_report, order_qty=100, symbol='AAPL', order_type='1', tif='0')
-        self.assertEqual(exec_report.getField(39), '8') 
-  
+        orig_cl_ord_id = self.app.last_execution_report.getField(11)
+        self.send_order_cancel_request(orig_cl_ord_id)
 
 if __name__ == '__main__':
     unittest.main()
-
