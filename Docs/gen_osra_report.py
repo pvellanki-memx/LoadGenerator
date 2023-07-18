23fe18dc80910801b48cb8ede1a67fed6649df2b
pvell <79489362+pvell@users.noreply.github.com>
2023-07-18 13:29:30 -0400

    Update gen_osra_report.py

--- Docs/gen_osra_report.py
+++ Docs/gen_osra_report.py
@@ -1,47 +1,45 @@
+
+
 import xml.etree.ElementTree as ET
 import pandas as pd
+import datetime
+
+# Read the XML data from trade.xml and parse it into a DataFrame (replace 'trade.xml' with the actual file path)
+tree_trade = ET.parse('trade_bak.xml')
+root_trade = tree_trade.getroot()
+
+# Create lists to store the parsed trade data
+quantity_list = []
+side_list = []
+pty_id_list = []
+pty_r_list = []
+sub_id_list = []
+rpt_id_list = []
+ultimate_clearing_firm_list = []  # List to store Ultimate Clearing Firm
+entering_firm_col1_list = []  # List to store Entering Firm - Column 1
+entering_firm_col2_list = []  # List to store Entering Firm - Column 2
+
 
 # Function to determine Ultimate Clearing Firm
 def get_ultimate_clearing_firm(sub_id, pty_r, rpt_id):
-    if (sub_id == 'C' or sub_id == 'M' or sub_id == 'F') and pty_r == '14' and rpt_id in rpt_id_list:
-        return pty_id_list[rpt_id_list.index(rpt_id)]
-    elif (sub_id == 'C' or sub_id == 'M' or sub_id == 'F') and pty_r == '18' and rpt_id in rpt_id_list:
-        return pty_id_list[rpt_id_list.index(rpt_id)]
-    elif (sub_id == 'C' or sub_id == 'M' or sub_id == 'F') and pty_r == '1':
+    if (sub_id in ['C', 'M', 'F']) and (pty_r == '14') and (rpt_id in rpt_id_list):
         return pty_id_list[rpt_id_list.index(rpt_id)]
     else:
         return None
 
-
-# Function to determine Entering Firm - Buy
-def get_entering_firm_buy(pty_r, pty_id,rpt_id):
-    if pty_r in ['1', '18'] and rpt_id in rpt_id_list:
-        return pty_id[:3] if pty_id else None
+# Function to determine Entering Firm - Column 1
+def get_entering_firm_col1(sub_id, pty_r, rpt_id):
+    if (sub_id in ['C', 'M', 'F']) and (pty_r in ['18', '1']) and (rpt_id in rpt_id_list):
+        return pty_id_list[rpt_id_list.index(rpt_id)][:3] if pty_id_list[rpt_id_list.index(rpt_id)] else None
     else:
         return None
 
-# Function to determine Entering Firm - Sell
-def get_entering_firm_sell(pty_r, pty_id,rpt_id):
-    if pty_r in ['2', '26'] and rpt_id in rpt_id_list:
-        return pty_id[:4] if pty_id else None
+# Function to determine Entering Firm - Column 2
+def get_entering_firm_col2(sub_id, pty_r, rpt_id):
+    if (sub_id in ['C', 'M', 'F']) and (pty_r in ['2', '26']) and (rpt_id in rpt_id_list):
+        return pty_id_list[rpt_id_list.index(rpt_id)][:4] if pty_id_list[rpt_id_list.index(rpt_id)] else None
     else:
         return None
-
-
-# Parse the XML data from trade.xml
-tree_trade = ET.parse('trade.xml')
-root_trade = tree_trade.getroot()
-
-# Create lists to store the parsed trade data
-quantity_list = []
-side_list = []
-pty_id_list = []
-pty_r_list = []
-sub_id_list = []
-rpt_id_list = []
-ultimate_clearing_firm_list = []  # List to store Ultimate Clearing Firm
-entering_firm_buy_list = []  # List to store Entering Firm - Buy
-entering_firm_sell_list = []  # List to store Entering Firm - Sell
 
 # Parse the XML data from trade.xml
 for trd_capt_rpt in root_trade.findall('TrdCaptRpt'):
@@ -65,48 +63,13 @@ for trd_capt_rpt in root_trade.findall('TrdCaptRpt'):
         ultimate_clearing_firm = get_ultimate_clearing_firm(sub_id, pty_r, rpt_id)
         ultimate_clearing_firm_list.append(ultimate_clearing_firm)
 
-        # Determine the Entering Firm - Buy
-        entering_firm_buy = get_entering_firm_buy(pty_r, pty_id,rpt_id)
-        entering_firm_buy_list.append(entering_firm_buy)
+        # Determine the Entering Firm - Column 1
+        entering_firm_col1 = get_entering_firm_col1(sub_id, pty_r, rpt_id)
+        entering_firm_col1_list.append(entering_firm_col1)
 
-        # Determine the Entering Firm - Sell
-        entering_firm_sell = get_entering_firm_sell(pty_r,pty_id, rpt_id)
-        entering_firm_sell_list.append(entering_firm_sell)
-
-# Parse the XML data from postmove.xml
-tree_postmove = ET.parse('postmove.xml')
-root_postmove = tree_postmove.getroot()
-
-# Parse the XML data from postmove.xml and append to the respective lists
-for trd_capt_rpt in root_postmove.findall('TrdCaptRpt'):
-    rpt_id = trd_capt_rpt.get('RptID')
-    side = trd_capt_rpt.find('RptSide').get('Side')
-
-    for pty in trd_capt_rpt.findall('RptSide/Pty'):
-        pty_id = pty.get('ID')
-        pty_r = pty.get('R')
-        sub_id = pty.find('Sub').get('ID') if pty.find('Sub') is not None else None
-
-        # Append the parsed data to the respective lists
-        quantity_list.append(trd_capt_rpt.get('LastQty'))
-        side_list.append(side)
-        pty_id_list.append(pty_id)
-        pty_r_list.append(pty_r)
-        sub_id_list.append(sub_id)
-        rpt_id_list.append(rpt_id)
-
-        # Determine the Ultimate Clearing Firm
-        ultimate_clearing_firm = get_ultimate_clearing_firm(sub_id, pty_r, rpt_id)
-        ultimate_clearing_firm_list.append(ultimate_clearing_firm)
-
-        # Determine the Entering Firm - Buy
-        entering_firm_buy = get_entering_firm_buy(pty_r,pty_id,rpt_id)
-        entering_firm_buy_list.append(entering_firm_buy)
-
-        # Determine the Entering Firm - Sell
-        entering_firm_sell = get_entering_firm_sell(pty_r, pty_id,rpt_id)
-        entering_firm_sell_list.append(entering_firm_sell)
-
+        # Determine the Entering Firm - Column 2
+        entering_firm_col2 = get_entering_firm_col2(sub_id, pty_r, rpt_id)
+        entering_firm_col2_list.append(entering_firm_col2)
 # Create the DataFrame
 trade_data_frame = pd.DataFrame({
     'Quantity': quantity_list,
@@ -116,121 +79,47 @@ trade_data_frame = pd.DataFrame({
     'Sub ID': sub_id_list,
     'RptID': rpt_id_list,
     'Ultimate Clearing Firm': ultimate_clearing_firm_list,
-    'Entering Firm - Buy': entering_firm_buy_list,
-    'Entering Firm - Sell': entering_firm_sell_list
+    'Entering Firm - Column 1': entering_firm_col1_list,
+    'Entering Firm - Column 2': entering_firm_col2_list
 })
 
-# Print the trade data DataFrame
-print(trade_data_frame)
+# Filter rows where at least one of Ultimate Clearing Firm, Entering Firm - Column 1, or Entering Firm - Column 2 is not None
+filtered_trade_data_frame = trade_data_frame[
+    trade_data_frame[['RptID','Sub ID','Side','Ultimate Clearing Firm', 'Entering Firm - Column 1', 'Entering Firm - Column 2']].notna().any(axis=1)
+]
 
-# Generate the timestamp for the file name
-import datetime
-timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M')
-
-# Write the DataFrame to an Excel file
-output_file_name = f'trade_data_{timestamp}.xlsx'
-trade_data_frame.to_excel(output_file_name, index=False)
-
-
-# Parse the XML data from trade.xml
-tree_trade = ET.parse('trade.xml')
-root_trade = tree_trade.getroot()
-
-# Create lists to store the parsed trade data
-quantity_list = []
-side_list = []
-pty_id_list = []
-pty_r_list = []
-sub_id_list = []
-rpt_id_list = []
-ultimate_clearing_firm_list = []  # List to store Ultimate Clearing Firm
-entering_firm_buy_list = []  # List to store Entering Firm - Buy
-entering_firm_sell_list = []  # List to store Entering Firm - Sell
-
-# Parse the XML data from trade.xml
-for trd_capt_rpt in root_trade.findall('TrdCaptRpt'):
-    rpt_id = trd_capt_rpt.get('RptID')
-    side = trd_capt_rpt.find('RptSide').get('Side')
-
-    for pty in trd_capt_rpt.findall('RptSide/Pty'):
-        pty_id = pty.get('ID')
-        pty_r = pty.get('R')
-        sub_id = pty.find('Sub').get('ID') if pty.find('Sub') is not None else None
-
-        # Append the parsed data to the respective lists
-        quantity_list.append(trd_capt_rpt.get('LastQty'))
-        side_list.append(side)
-        pty_id_list.append(pty_id)
-        pty_r_list.append(pty_r)
-        sub_id_list.append(sub_id)
-        rpt_id_list.append(rpt_id)
-
-        # Determine the Ultimate Clearing Firm
-        ultimate_clearing_firm = get_ultimate_clearing_firm(sub_id, pty_r, rpt_id)
-        ultimate_clearing_firm_list.append(ultimate_clearing_firm)
-
-        # Determine the Entering Firm - Buy
-        entering_firm_buy = get_entering_firm_buy(pty_r, pty_id,rpt_id)
-        entering_firm_buy_list.append(entering_firm_buy)
-
-        # Determine the Entering Firm - Sell
-        entering_firm_sell = get_entering_firm_sell(pty_r, pty_id,rpt_id)
-        entering_firm_sell_list.append(entering_firm_sell)
-
-# Parse the XML data from postmove.xml
-tree_postmove = ET.parse('postmove.xml')
-root_postmove = tree_postmove.getroot()
-
-# Parse the XML data from postmove.xml and append to the respective lists
-for trd_capt_rpt in root_postmove.findall('TrdCaptRpt'):
-    rpt_id = trd_capt_rpt.get('RptID')
-    side = trd_capt_rpt.find('RptSide').get('Side')
-
-    for pty in trd_capt_rpt.findall('RptSide/Pty'):
-        pty_id = pty.get('ID')
-        pty_r = pty.get('R')
-        sub_id = pty.find('Sub').get('ID') if pty.find('Sub') is not None else None
-
-        # Append the parsed data to the respective lists
-        quantity_list.append(trd_capt_rpt.get('LastQty'))
-        side_list.append(side)
-        pty_id_list.append(pty_id)
-        pty_r_list.append(pty_r)
-        sub_id_list.append(sub_id)
-        rpt_id_list.append(rpt_id)
-
-        # Determine the Ultimate Clearing Firm
-        ultimate_clearing_firm = get_ultimate_clearing_firm(sub_id, pty_r, rpt_id)
-        ultimate_clearing_firm_list.append(ultimate_clearing_firm)
-
-        # Determine the Entering Firm - Buy
-        entering_firm_buy = get_entering_firm_buy(pty_r, pty_id,rpt_id)
-        entering_firm_buy_list.append(entering_firm_buy)
-
-        # Determine the Entering Firm - Sell
-        entering_firm_sell = get_entering_firm_sell(pty_r, pty_id,rpt_id)
-        entering_firm_sell_list.append(entering_firm_sell)
-
-# Create the DataFrame
-trade_data_frame = pd.DataFrame({
-    'Quantity': quantity_list,
-    'Side': side_list,
-    'Pty ID': pty_id_list,
-    'Pty R': pty_r_list,
-    'Sub ID': sub_id_list,
-    'RptID': rpt_id_list,
-    'Ultimate Clearing Firm': ultimate_clearing_firm_list,
-    'Entering Firm - Buy': entering_firm_buy_list,
-    'Entering Firm - Sell': entering_firm_sell_list
+# Group the filtered DataFrame and aggregate the values for Ultimate Clearing Firm, Entering Firm - Column 1, and Entering Firm - Column 2
+grouped_trade_data_frame = filtered_trade_data_frame.groupby(['Side', 'Sub ID', 'RptID'], as_index=False).agg({
+    'Ultimate Clearing Firm': 'first',
+    'Entering Firm - Column 1': 'first',
+    'Entering Firm - Column 2': 'first',
+    
 })
 
-# Print the trade data DataFrame
-print(trade_data_frame)
+print(grouped_trade_data_frame)
 
-# Generate the timestamp for the file name
-import datetime
+# Replace 'None' with a placeholder value (e.g., 'Unknown')
+trade_data_frame.fillna('Unknown', inplace=True)
+
+# Group the data by the desired columns and calculate the count for each group
+pivoted_trade_data_frame = trade_data_frame.groupby(['Side', 'Sub ID', 'Ultimate Clearing Firm', 'Entering Firm - Column 1', 'Entering Firm - Column 2'], as_index=False).size()
+pivoted_trade_data_frame = pivoted_trade_data_frame.reset_index(name='Total Qty')
+
+# Print the grouped trade data DataFrame
+print(pivoted_trade_data_frame)
+
+# Generate the timestamp for the file name up to minutes
 timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M')
 
-# Write the DataFrame to an Excel file
-output_file_name = f'trade_data_{timestamp}.xlsx'
-trade_data_frame.to_excel(output_file_name, index=False)
+# Write the grouped_trade_data_frame and pivoted_trade_data_frame to an Excel file with separate sheets
+output_file_name = f'grouped_and_pivoted_trade_data_{timestamp}.xlsx'
+
+with pd.ExcelWriter(output_file_name) as writer:
+    grouped_trade_data_frame.to_excel(writer, sheet_name='Grouped Trade Data', index=False)
+    pivoted_trade_data_frame.to_excel(writer, sheet_name='Pivoted Trade Data', index=False)
+
+print(f"Data has been written to {output_file_name}")
+
+
+
+
