6a0ce5c13645b3d16f253c4dd0d01ef8971da4be
pvell <79489362+pvell@users.noreply.github.com>
2023-07-21 10:06:18 -0400

    Update gen_osra_report.py

--- Docs/gen_osra_report.py
+++ Docs/gen_osra_report.py
@@ -3,8 +3,6 @@ import pandas as pd
 import numpy as np
 import datetime
 
-
-# Function to determine Ultimate Clearing Firm
 def get_ultimate_clearing_firm(sub_id, pty_r, rpt_id):
     if sub_id in ['C', 'M', 'F']:
         for i in range(len(pty_r_list)):
@@ -35,117 +33,131 @@ def get_entering_firm_col2(sub_id, pty_r, rpt_id):
     return None
 
 
-def process_batch(batch_data):
-    quantity_list = []
-    side_list = []
-    pty_id_list = []
-    pty_r_list = []
-    sub_id_list = []
-    rpt_id_list = []
-    ultimate_clearing_firm_list = []
-    entering_firm_col1_list = []
-    entering_firm_col2_list = []
-
-    for trd_capt_rpt in batch_data:
-        rpt_id = trd_capt_rpt.get('RptID')
-        for rpt_side in trd_capt_rpt.findall('RptSide'):
-            side = rpt_side.get('Side')
-            for pty in rpt_side.findall('Pty'):
-                pty_id = pty.get('ID')
-                pty_r = pty.get('R')
-                sub_id = pty.find('Sub').get('ID') if pty.find('Sub') is not None else None
-
-                quantity_list.append(int(trd_capt_rpt.get('LastQty')))
-                side_list.append(side)
-                pty_id_list.append(pty_id)
-                pty_r_list.append(pty_r)
-                sub_id_list.append(sub_id)
-                rpt_id_list.append(rpt_id)
-
-                ultimate_clearing_firm = get_ultimate_clearing_firm(sub_id, pty_r, rpt_id)
-                ultimate_clearing_firm_list.append(ultimate_clearing_firm)
-
-                entering_firm_col1 = get_entering_firm_col1(sub_id, pty_r, rpt_id)
-                entering_firm_col1_list.append(entering_firm_col1)
-
-                entering_firm_col2 = get_entering_firm_col2(sub_id, pty_r, rpt_id)
-                entering_firm_col2_list.append(entering_firm_col2)
-
-    batch_df = {
-        'Quantity': quantity_list,
-        'Side': side_list,
-        'Pty ID': pty_id_list,
-        'Pty R': pty_r_list,
-        'Sub ID': sub_id_list,
-        'RptID': rpt_id_list,
-        'Ultimate Clearing Firm': ultimate_clearing_firm_list,
-        'Entering Firm - Column 1': entering_firm_col1_list,
-        'Entering Firm - Column 2': entering_firm_col2_list
-    }
-
-    return batch_df
-
-# Read the XML data from trade.xml and parse it into NumPy arrays (replace 'trade.xml' with the actual file path)
-print("Parsing the XML data...")
-tree_trade = ET.iterparse('trade_bak.xml', events=('start', 'end'))
-_, root_trade = next(tree_trade)  # Get the root element
-
-# Initialize lists to store the parsed trade data
-quantity_list = []
-side_list = []
-pty_id_list = []
-pty_r_list = []
-sub_id_list = []
-rpt_id_list = []
-ultimate_clearing_firm_list = []
-entering_firm_col1_list = []
-entering_firm_col2_list = []
-
-batch_size = 10000
-batch_data = []
-processed_elements = 0
+# Initialize an empty DataFrame to store the parsed trade data
+trade_data_frame = pd.DataFrame()
 
 # Create lists to store the parsed trade data
 batch_size = 10000
 batch_data = []
 processed_elements = 0
 
-# Parse the XML data from trade.xml
-for event, element in tree_trade:
-    if event == 'end' and element.tag == 'TrdCaptRpt':
-        batch_data.append(element)
-        processed_elements += 1
 
-        # Process a batch of data
-        if processed_elements % batch_size == 0:
-            print(f"Processing batch {processed_elements // batch_size}")
-            batch_df = process_batch(batch_data)
-            batch_data.clear()
+# Read the XML data from trade.xml and parse it into NumPy arrays (replace 'trade.xml' with the actual file path)
+print("Parsing the XML data...")
+tree_trade = ET.parse('trade_bak.xml')
+root_trade = tree_trade.getroot()
 
-            if processed_elements == batch_size:
-                # Create the DataFrame with the first batch
-                trade_data_frame = pd.DataFrame(batch_df)
-            else:
-                # Append to the DataFrame for subsequent batches
-                trade_data_frame = trade_data_frame.append(pd.DataFrame(batch_df))
+# Create lists to store the parsed trade data
+quantity_list = []
+side_list = []
+pty_id_list = []
+pty_r_list = []
+sub_id_list = []
+rpt_id_list = []
+ultimate_clearing_firm_list = []  # List to store Ultimate Clearing Firm
+entering_firm_col1_list = []  # List to store Entering Firm - Column 1
+entering_firm_col2_list = []  # List to store Entering Firm - Column 2
 
-            # Clear memory by removing processed elements
-            root_trade.clear()
+# Loop through the TrdCaptRpt elements and parse the data
+print("Processing the XML data...")
+for trd_capt_rpt in root_trade.findall('.//TrdCaptRpt'):
+    rpt_id = trd_capt_rpt.get('RptID')
+    for rpt_side in trd_capt_rpt.findall('RptSide'):
+        side = rpt_side.get('Side')
+        for pty in rpt_side.findall('Pty'):
+            pty_id = pty.get('ID')
+            pty_r = pty.get('R')
+            sub_id = pty.find('Sub').get('ID') if pty.find('Sub') is not None else None
+
+            # Append the parsed data to the respective lists
+            quantity_list.append(int(trd_capt_rpt.get('LastQty')))  # Convert quantity to integer
+            side_list.append(side)
+            pty_id_list.append(pty_id)
+            pty_r_list.append(pty_r)
+            sub_id_list.append(sub_id)
+            rpt_id_list.append(rpt_id)
+
+            # Determine the Ultimate Clearing Firm
+            ultimate_clearing_firm = get_ultimate_clearing_firm(sub_id, pty_r, rpt_id)
+            ultimate_clearing_firm_list.append(ultimate_clearing_firm)
+
+            # Determine the Entering Firm - Column 1
+            entering_firm_col1 = get_entering_firm_col1(sub_id, pty_r, rpt_id)
+            entering_firm_col1_list.append(entering_firm_col1)
+
+            # Determine the Entering Firm - Column 2
+            entering_firm_col2 = get_entering_firm_col2(sub_id, pty_r, rpt_id)
+            entering_firm_col2_list.append(entering_firm_col2)
+
+    # Process the data in batches (same as before)
+    if len(quantity_list) >= batch_size:
+        print(f"Processing batch {processed_elements // batch_size}")
+        batch_df = process_batch({
+            'Quantity': np.array(quantity_list),
+            'Side': np.array(side_list),
+            'Pty ID': np.array(pty_id_list),
+            'Pty R': np.array(pty_r_list),
+            'Sub ID': np.array(sub_id_list),
+            'RptID': np.array(rpt_id_list),
+            'Ultimate Clearing Firm': np.array(ultimate_clearing_firm_list),
+            'Entering Firm - Column 1': np.array(entering_firm_col1_list),
+            'Entering Firm - Column 2': np.array(entering_firm_col2_list)
+        })
+
+        if processed_elements == 0:
+            # Create the DataFrame for the first batch
+            trade_data_frame = pd.DataFrame(batch_df)
+        else:
+            # Append to the DataFrame for subsequent batches
+            trade_data_frame = trade_data_frame.append(pd.DataFrame(batch_df))
+
+        # Clear the lists for the next batch
+        quantity_list.clear()
+        side_list.clear()
+        pty_id_list.clear()
+        pty_r_list.clear()
+        sub_id_list.clear()
+        rpt_id_list.clear()
+        ultimate_clearing_firm_list.clear()
+        entering_firm_col1_list.clear()
+        entering_firm_col2_list.clear()
+
+    # Process the data in batches
+    if len(quantity_list) >= batch_size:
+        # ... (The batch processing and DataFrame creation remain unchanged as before)
+
+        # Clear the lists for the next batch
+        quantity_list.clear()
+        side_list.clear()
+        pty_id_list.clear()
+        pty_r_list.clear()
+        sub_id_list.clear()
+        rpt_id_list.clear()
+        ultimate_clearing_firm_list.clear()
+        entering_firm_col1_list.clear()
+        entering_firm_col2_list.clear()
 
 # Process the remaining data (if any)
-if batch_data:
+if quantity_list:
     print(f"Processing the last batch {processed_elements // batch_size + 1}")
-    batch_df = process_batch(batch_data)
-    
+    batch_df = process_batch({
+        'Quantity': np.array(quantity_list),
+        'Side': np.array(side_list),
+        'Pty ID': np.array(pty_id_list),
+        'Pty R': np.array(pty_r_list),
+        'Sub ID': np.array(sub_id_list),
+        'RptID': np.array(rpt_id_list),
+        'Ultimate Clearing Firm': np.array(ultimate_clearing_firm_list),
+        'Entering Firm - Column 1': np.array(entering_firm_col1_list),
+        'Entering Firm - Column 2': np.array(entering_firm_col2_list)
+    })
+
     if processed_elements == 0:
-        # Create the DataFrame if it's the first and only batch
-        trade_data_frame = pd.DataFrame(batch_df)
+        # Create an empty DataFrame if no data is processed
+        trade_data_frame = pd.DataFrame()
     else:
         # Append to the DataFrame for the last batch
-        if 'trade_data_frame' not in locals():
-            trade_data_frame = pd.DataFrame()
         trade_data_frame = trade_data_frame.append(pd.DataFrame(batch_df))
-
 
 # Filter rows where at least one of Ultimate Clearing Firm, Entering Firm - Column 1, or Entering Firm - Column 2 is not None
 print("Filtering the DataFrame...")
