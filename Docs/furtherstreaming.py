d59fbd1721a3f2fb46acc847e56dacb036890f08
pvell <79489362+pvell@users.noreply.github.com>
2023-09-11 10:34:35 -0400

    Update furtherstreaming.py

--- Docs/furtherstreaming.py
+++ Docs/furtherstreaming.py
@@ -1,7 +1,9 @@
 import sqlite3
+import sys
 
+db_name = sys.argv[1]
 # Connect to the SQLite database
-conn = sqlite3.connect('trades_81.db')
+conn = sqlite3.connect(db_name)
 
 # Drop the 'grouped_trades_summary' table if it exists
 conn.execute('DROP TABLE IF EXISTS grouped_trades_summary')
@@ -19,12 +21,14 @@ conn.execute('''CREATE TABLE IF NOT EXISTS grouped_trades_summary (
                     PREVLY_RPTD TEXT,
                     RPT_TYP TEXT,
                     MTCH_STAT TEXT,
+                    TRD_SUB_TYPE TEXT,
+                    TRD_DT TEXT,
                     Total_Quantity INTEGER
                  )''')
 
 # Query the data from the 'grouped_trades' table with grouping and summing the quantities
 query_grouped = """
-    INSERT INTO grouped_trades_summary (Side, Sub_ID, Ultimate_Clearing_Firm, Entering_Firm_Col1, EXCH, TRANS_TYPE,  BIZ_DT, CFI, PREVLY_RPTD, RPT_TYP,MTCH_STAT, Total_Quantity)
+    INSERT INTO grouped_trades_summary (Side, Sub_ID, Ultimate_Clearing_Firm, Entering_Firm_Col1, EXCH, TRANS_TYPE,  BIZ_DT, CFI, PREVLY_RPTD, RPT_TYP,MTCH_STAT,TRD_SUB_TYPE,TRD_DT, Total_Quantity)
     SELECT 
         Side,
         Sub_ID,
@@ -37,6 +41,8 @@ query_grouped = """
         PREVLY_RPTD,
         RPT_TYP,
         MTCH_STAT,
+        TRD_SUB_TYPE,
+        TRD_DT,
         SUM(Quantity) AS Total_Quantity
     FROM grouped_trades
     GROUP BY Side, Sub_ID, Ultimate_Clearing_Firm, Entering_Firm_Col1, EXCH, TRANS_TYPE, BIZ_DT, CFI, PREVLY_RPTD, RPT_TYP,MTCH_STAT
